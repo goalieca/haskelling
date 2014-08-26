9cf312853b009b519792653c79f9d0640dcbb24d
Ryan Dickie <goalieca@gmail.com>
2014-08-26 17:34:23 -0400

    Fixed bug in pandigital concatenation

--- 038.hs
+++ 038.hs
@@ -20,13 +20,22 @@ import qualified Data.HashSet as HashSet
 (*|) a b = a * 10 + b
 undigits xs = foldl (*|) 0 xs
 
+--copied from 034.hs and reversed to be in right order
+digits = reverse . map (`mod` 10) . takeWhile (> 0) . iterate (`div` 10)
+
 -- 1 to 9 pandigitals.. all! sorted from largest to smallest
 is_pandigital::Int->Bool
 is_pandigital x = x `HashSet.member` pandigitals
         where pandigitals = HashSet.fromList (map undigits (permutations [1..9]))
 
+num_digits x = _rec 1 10
+    where _rec s y
+            | y < x = _rec (s+1) (y*10)
+            | otherwise = s
+
 -- make a candidate number
 nums::Int->Int
-nums x = read (take 9 $ concat $ map (show . (*) x) [1..])::Int
+nums x = undigits $ concat $ map (digits . (*) x) [1..n] 
+    where n = 9 `div` (num_digits x)
 
 main = print $ maximum $ filter is_pandigital $ map nums [1..9999]
